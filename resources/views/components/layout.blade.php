e81b3fa89aa7b329fc566168588932347c7e8100
Henry Benaye <henry.be@outlook.com>
2023-04-16 20:09:56 +0200

    Added tournament beacket

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,4 +3,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/underscore@1.13.6/underscore-umd-min.js "></script>
 </head>
