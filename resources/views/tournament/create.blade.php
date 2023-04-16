d764e5cc54f6a865c4642aa71092b4ccd3c5b5eb
Henry Benaye <henry.be@outlook.com>
2023-04-16 18:18:18 +0200

    form setup

--- resources/views/tournament/create.blade.php
+++ resources/views/tournament/create.blade.php
@@ -5,44 +5,25 @@
 </x-layout>
 <body class="antialiased h-full">
 <div class="w-full h-full flex justify-center items-center flex-col">
-    <form id="form">
-        <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="name@flowbite.com" required>
-        </div>
-        <div class="mb-6">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                password</label>
-            <input type="password" id="password"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   required>
-        </div>
-        <div class="flex items-start mb-6">
-            <div class="flex items-center h-5">
-                <input id="remember" type="checkbox" value=""
-                       class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                       required>
-            </div>
-            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+    <form id="form" class="border-solid border-2 border-black rounded-md p-4 shadow-xl">
+        <div id="inputTeams" class="mb-4">
+
         </div>
         <button type="submit"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Submit
         </button>
     </form>
-
 </div>
 <script>
     for (let i = 0; i < {{$amountTeams}}; i++) {
-        const list = document.getElementById("form");
+        const list = document.getElementById("inputTeams");
         list.innerHTML +=
             `<div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email"
+                    <label for="teamName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Team Naam #${i}</label>
+            <input type="text" id="teamName"
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="name@flowbite.com" required>
+                    required>
             </div>`;
     }
 </script>
